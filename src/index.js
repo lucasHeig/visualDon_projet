f51b4465c4cdd28c537dc9303868926968ce0c56
Elodie Perring <elodie.perring@heig-vd-ch>
2024-03-14 15:54:41 +0100

    initialisation du projet

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Pour importer les données vous pouvez soit importer directement les csv (@rollup/plugin-dsv), soit utiliser la méthode csv de d3-fetch
+
+// @rollup/plugin-dsv
+import imdb from "../data/IMDB.csv";
+
+
+console.log(imdb);
