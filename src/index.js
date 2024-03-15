d9ae6498e48896fe6c5de6edef6f7c4a86060f9b
Elodie Perring <elodie.perring@heig-vd-ch>
2024-03-15 13:22:24 +0100

    modification projet name to tv_shows in package.json

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
 // @rollup/plugin-dsv
 import imdb from "../data/IMDB.csv";
 
-
 console.log(imdb);
+
+console.log(imdb[0].Description);
+console.log(imdb[0].Episodes);
+console.log(imdb[0].ImageSrc);
+console.log(imdb[0].href);
+console.log(imdb[0].Type);
+console.log(imdb[0].Year);
+console.log(imdb[0]['"Name"']);
